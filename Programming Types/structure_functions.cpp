8cf97471d50a54d8c7018ad4dd4a094a47c44eca
ramdhanishivam <ramdhanishivam7678@gmail.com>
2021-10-14 07:30:03 -0700

    added initilize fucntion.

--- Programming Types/structure_functions.cpp
+++ Programming Types/structure_functions.cpp
@@ -14,10 +14,17 @@ using namespace std;
 //    Now, we know that the length & breadth are the properties of a rectangle.
 //    Why not combine thier declaration into a structure.
 
+//    This program depicts how structure & functions lead to better programming way.
+
 struct Rectangle {
     int length;
     int breadth;
 };
+
+void initialize(struct Rectangle *r, int l, int b){
+    r->length = l;
+    r->breadth = b;
+}
 
 int area(struct Rectangle r){
     return r.length*r.breadth;
@@ -30,9 +37,12 @@ int perimeter(struct Rectangle r){
 
 int main() {
     struct Rectangle r;
+    int l,b;
     
     cout<<"Enter length & breadth of rectangle";
-    cin>>r.length>>r.breadth;
+    cin>>l>>b;
+    
+    initialize(&r, l, b);
     
     int a = area(r);
     int peri = perimeter(r);
