86390b4f7dd3e1a390c1fd3cbd91705930415881
ramdhanishivam <ramdhanishivam7678@gmail.com>
2021-10-16 15:56:05 -0700

    Static/Global Variable used in recursive function.

--- Recursion/static_global_variable_recursion.cpp
+++ Recursion/static_global_variable_recursion.cpp
@@ -1,5 +1,5 @@
 //
-//  tailhead2.cpp
+//  static_global_variable_recursion.cpp
 //  DSA_Basics
 //
 //  Created by Shivam Ramdhani on 16/10/21.
@@ -11,9 +11,15 @@
 
 using namespace std;
 
+// int x = 5 // Global Variable
+// There will only one copy of static/global created
+// number of copies are created for other types of variable.
+
 int func1(int n){
+    static int x;
     if (n>0) {
-        return func1(n-1)+n;
+        x++;
+        return func1(n-1)+x;
     }
     return 0;
 }
