6eb3dc0586b567b02f8a5353f8f7568d5ba0c4db
ramdhanishivam <ramdhanishivam7678@gmail.com>
2021-10-16 18:34:33 -0700

    Tail Recustion compared along with loops.

---
+++ Recursion/tail_recursion.cpp
@@ -0,0 +1,47 @@
+//
+//  tail_recursion.cpp
+//  DSA_Basics
+//
+//  Created by Shivam Ramdhani on 16/10/21.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+// Tail Recusion can be easily converted into the form of loops.
+// Both functions look much similar.
+
+// Intresting Point to know, few compilers under the hood convert the Tail recursion
+// into loops, thus the space complexity overhead is optimized by compiler itself.
+
+// Function using recursion.
+// space complexity - O(n)
+// time complexity - O(n)
+void func1(int n){
+    if (n>0) {
+        cout<<n<<" ";
+        func1(n-1);
+    }
+}
+
+// Function using loop.
+// space complexity - O(1)
+// time complexity - O(n)
+void func2(int n){
+    while (n>0) {
+        cout<<n<<" ";
+        n--;
+    }
+}
+
+int main() {
+    int x = 3;
+    func1(x);
+    cout<<endl;
+    func1(x);
+    cout<<endl;
+    return 0;
+}
