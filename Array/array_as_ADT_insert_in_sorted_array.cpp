229c68570db76a461dd3b5c347411501ae1d4a24
ramdhanishivam <ramdhanishivam7678@gmail.com>
2021-10-23 01:19:55 -0700

    Added new function for InsertionSort.

--- Array/array_as_ADT_insert_in_sorted_array.cpp
+++ Array/array_as_ADT_insert_in_sorted_array.cpp
@@ -36,9 +36,22 @@ void insert_in_sorted_array(struct Array *arr, int value){
     arr->A[i+1] = value; // inserting element in its position.
 }
 
+void insert_in_sorted_array2(struct Array *arr, int value){
+    int i = arr->length - 1; // starting with the last element
+    if (arr->length == arr->size) {
+        return;
+    }
+    while (arr->A[i] > value) {
+        arr->A[i+1] = arr->A[i]; // shifting left element to right
+        i--;
+    }
+    arr->A[i+1] = value; // inserting element in its position.
+    arr->length++; // incrementing the length as new element is to inserted.
+}
+
 int main() {
     struct Array arr={{1,2,4,5,6}, 20, 5};
-    insert_in_sorted_array(&arr, 3);
+    insert_in_sorted_array2(&arr, 3);
     display(arr);
     return 0;
 }
