28f13ebca4bfca9fe5ba27f4eb45a033090b0677
ramdhanishivam <ramdhanishivam7678@gmail.com>
2021-10-28 16:05:05 -0700

    Added comments.

--- Strings/check_palindrome_string.cpp
+++ Strings/check_palindrome_string.cpp
@@ -14,19 +14,22 @@ int main(){
     char A[] = "madam";
     char B[10];
     int i,j;
+    // Here we figure out the length of string. i.e 'i'
     for (i=0; A[i] != '\0'; i++) {}
-    i = i-1;
+    i = i-1; // length of the string
     for (j=0; i>=0; j++,i--) {
-        B[j] = A[i];
+        B[j] = A[i]; // store the reverse of string in a array. i.e 'B'
     }
     cout<<B<<endl;
     int x,y;
+    // In below loop we compare whether each character of both string A & B are equal or not.
     for (x=0,y=0; A[x] !='\0' && B[y] !='\0'; x++,y++) {
         if (A[x] != B[y]) {
             break;
         }
     }
     
+    // Final Check for Palindrome.
     if (A[x] == B[y]) {
         cout<<"Palindrome!!"<<endl;
     } else {
