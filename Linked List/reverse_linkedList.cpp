66b0674c827ce67c5cd704666924e5145aed1068
ramdhanishivam <ramdhanishivam7678@gmail.com>
2021-11-09 14:06:50 -0800

    Reverse LL using Sliding Pointers.

---
+++ Linked List/reverse_linkedList.cpp
@@ -0,0 +1,71 @@
+//
+//  reverse_linkedList.cpp
+//  DSA_Basics
+//
+//  Created by Shivam Ramdhani on 09/11/21.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first = NULL, *last_node = NULL;
+
+// This function will always insert a new node to the last of LL.
+// Also, it will help in creating a LL with zero elements.
+void create(int value)
+{
+    struct Node *new_node;
+    new_node = new Node;
+    new_node->data = value;
+    new_node->next = NULL;
+    if (first == NULL) {
+        first = last_node = new_node;
+    } else {
+        last_node->next = new_node;
+        last_node = new_node;
+    }
+    
+}
+
+void display(struct Node *linked_list_node)
+{
+    while (linked_list_node) {
+        cout<<linked_list_node->data<<",";
+        linked_list_node = linked_list_node->next;
+    }
+}
+
+// function uses the method of sliding pointers to reverse the list.
+void reverse_linkedList(struct Node *p){
+    struct Node *q = NULL;
+    struct Node *r = NULL;
+    
+    while (p) {
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = r;
+    }
+    first = q;
+}
+
+int main()
+{
+    string result;
+    create(10);
+    create(20);
+    create(30);
+    create(40);
+    display(first);
+    cout<<endl;
+    reverse_linkedList(first);
+    display(first);
+    cout<<endl;
+    return 0;
+}
